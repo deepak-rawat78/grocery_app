76c203d11013bbc56695f5a46e57a5f449383d1c
Deepak Rawat <deepakrawat7789@gmail.com>
2023-11-24 00:05:29 +0530

    product detail page updated

--- src/views/productDetail/ProductDetail.styles.ts
+++ src/views/productDetail/ProductDetail.styles.ts
@@ -1,19 +1,82 @@
 import {StyleSheet} from 'react-native';
+import colors from '../../assets/colors';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
   headerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginHorizontal: 20,
   },
-  rightArrow: {height: 18, width: 10},
+  rightArrow: {height: 11, width: 6},
   bagIcon: {height: 18, width: 16},
   backButton: {
     height: 40,
     width: 40,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: colors.greyScaleBlack05,
+    borderRadius: 20,
+  },
+  primaryTitle: {
+    fontSize: 50,
+    lineHeight: 62,
+    color: colors.greyScaleBlack,
+    fontWeight: '300',
+    marginHorizontal: 20,
+  },
+  secondaryTitle: {
+    fontWeight: '800',
+  },
+  buyNowButton: {
+    width: '48%',
+    backgroundColor: colors.priamryLightBlue,
+  },
+  buyNowText: {
+    color: colors.white,
+  },
+  addToCartButton: {width: '40%'},
+  buttonContainer: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  chip: {
+    marginLeft: 14,
+  },
+  priceValue: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.priamryLightBlue,
+    fontWeight: '700',
+  },
+  priceText: {
+    fontWeight: '400',
+    color: colors.priamryLightBlue,
+  },
+  detailHeading: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.greyScaleBlack,
+    fontWeight: '400',
+    marginTop: 30,
+  },
+  detailText: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.greyScaleBlack03,
+    fontWeight: '400',
+  },
+  detailContainer: {
+    marginHorizontal: 20,
   },
 });
 
--- src/views/productDetail/ProductDetail.tsx
+++ src/views/productDetail/ProductDetail.tsx
@@ -1,8 +1,10 @@
-import {View, TouchableOpacity, Image} from 'react-native';
+import {View, TouchableOpacity, Image, Text} from 'react-native';
 import React from 'react';
 import {bagIcon, rightArrow} from '../../assets/icons';
 import styles from './ProductDetail.styles';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import CButton from '../../components/CButton/CButton';
+import Chip from '../../components/Chip/Chip';
 
 const Header = () => {
   const {top} = useSafeAreaInsets();
@@ -21,8 +23,36 @@ const Header = () => {
 
 const ProductDetail = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
+      <Text style={styles.primaryTitle}>
+        Thin Choise {'\n'}
+        <Text style={styles.secondaryTitle}>Top Orange</Text>
+      </Text>
+      <View style={styles.detailContainer}>
+        <View style={styles.priceContainer}>
+          <Text style={styles.priceText}>
+            <Text style={styles.priceValue}>$34</Text>/KG
+          </Text>
+          <Chip value="$22.04 OFF" containerStyle={styles.chip} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <CButton
+            title="Add To Cart"
+            containerStyle={styles.addToCartButton}
+          />
+          <CButton
+            title="Buy Now"
+            textStyle={styles.buyNowText}
+            containerStyle={styles.buyNowButton}
+          />
+        </View>
+        <Text style={styles.detailHeading}>Details</Text>
+        <Text style={styles.detailText}>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+          Nullam quis risus eget urna mollis ornare vel eu leo.
+        </Text>
+      </View>
     </View>
   );
 };
